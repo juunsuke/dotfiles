deae1cab4f74dafc63c28f5a1a26ef3ef78b4c3a
Juun <otajuun@gmail.com>
2023-10-25 14:15:16 -0400

    Update

---
+++ .config/mode3.sh
@@ -0,0 +1,2 @@
+xrandr --output DP-2 --mode 2560x1600 --rate 240 --output HDMI-0 --off
+nitrogen --restore
---
+++ .config/xinput.sh
@@ -0,0 +1,12 @@
+
+xset r rate 200 50
+xset m 0 0
+
+xinput set-prop "ASUE120D:00 04F3:31FB Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "ASUE120D:00 04F3:31FB Touchpad" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "ASUE120D:00 04F3:31FB Touchpad" "libinput Middle Emulation Enabled" 1
+
+xinput set-prop "MX Ergo" "Coordinate Transformation Matrix" 2 0 0 0 2 0 0 0 1
+xinput set-prop "MX Ergo" "libinput Accel Speed" 1
+
+
