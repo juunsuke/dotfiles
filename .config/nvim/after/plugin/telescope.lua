c6f6b34ed9afbf5f624019f906b3994e59aa71fc
Juun <otajuun@gmail.com>
2025-08-16 11:38:09 -0400

    nvim update

--- .config/nvim/after/plugin/telescope.lua
+++
@@ -1,6 +0,0 @@
-local tsc = require('telescope.builtin')
-vim.keymap.set("n", "<Tab>", function() tsc.buffers({sort_mru = true, ignore_current_buffer = true}) end, {})
-vim.keymap.set("n", "<C-f>", tsc.find_files, {})
-vim.keymap.set('n', '<C-g>', tsc.git_files, {})
-
-
--- .config/nvim/after/plugin/terminal.lua
+++
@@ -1,6 +0,0 @@
-require("terminal").setup {
-	layout = { open_cmd = "botright new" },
-	cmd = { vim.o.shell },
-	autoclose = false,
-}
-
